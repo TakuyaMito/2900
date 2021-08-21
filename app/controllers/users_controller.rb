6d341b76954f286bbbc5531466b5df7e8825a1f6
Takuya <dyhpaax@gmail.com>
2021-08-21 10:29:33 +0900

    userモデルバリデーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :last_name, :first_name)
+  end
 end
